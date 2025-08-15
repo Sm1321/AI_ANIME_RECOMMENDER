c68652e9ab8a1701dd2ecada81e83e37a8d0e6d6
Sm1321 <siddulamohan1321@gmail.com>
2025-08-15 13:53:17 +0530

    added

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,21 @@
-class Anime
+import pandas as pd
+
+
+class AnimeDataLoader:
+    def __init__(self,original_csv:str,processed_csv:str):
+        self.original_csv = original_csv
+        self.processed_csv = processed_csv
+
+
+    def load_and_process(self):
+        df = pd.read_csv(self.original,encoding  = 'utf-8',error_bad_lines = False).dropna()
+        requried_cols = {'Name','Genres','sypnopsis'}
+        missing = requried_cols - set(df.columns)
+        if missing:
+            raise ValueError("Missing Column in CSV File")
+        df['combined_info'] = (
+            "Title: " + df['Name'] + " Overview"
+        )
+        
+        
+
